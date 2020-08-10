509ff5a98016366dd5d5ced6e351c3f1391fb057
[endricott] <[egliute@hotmail.com]>
2020-08-10 23:19:11 +0200

    admin index add

---
+++ admin/adminpage.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>Amin Page</title>
+        <meta charset="UTF-8">
+        <link href="../styles/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="wrapper">
+            <h1>db options</h1>
+            <h3><a href="./calendarnotes/filmliste.php">Die Kalendarliste anzeigen</a></h3>
+            <h3><a href="./users/adressen-db.php">Benutzerliste anzeigen</a></h3>
+        </div>
+    </body>
+</html>
